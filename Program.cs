96c553a4ff84b66bbbf6b32556d44afdbd203ba7
Ergerica <ergerica76@gmail.com>
2019-07-05 13:43:26 -0400

    Uploading project files

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Universal_translator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
